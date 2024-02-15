c0ab91f6f065001de698ae22970ed5273be3a8a3
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2024-02-15 16:18:35 +0200

    update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        B instanceB = new B();
+        System.out.println(instanceB.b);
+        System.out.println(instanceB);
+        System.out.println(instanceB.a);
+//        System.out.println(.staticA);
+        System.out.println(instanceB.getClass().getFields());
+
+        /*System.out.println(0/0);
+        System.out.println("Hello world!");
+        int a = 123;
+        int[] array = {1, 2, 3};
+        String s = "ASD";
+        System.out.println(array.toString());
+        s.wait();
+        array.wait();*/
+    }
+}
+
+
+
+class A {
+    public int a = 1;
+    static int aStatic = 2;
+}
+
+class B extends A {
+    public int b = super.a;
 }
